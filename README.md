891472d12109b185a84e87750f82bfe09ca98c48
Joe Keenan <joe@flyingdiver.com>
2022-05-27 16:37:19 -0400

    Update to new PurpleAir API
    Changed URLs and response formats
    API Key now required

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Minimalistic Purple Air plugin for Indigo Home Automation system
 | Requires Local Network | No                  |   |
 | Requires Internet      | Yes                 |   |
 | Hardware Interface     | None                |   |
+
+## API Key Required
+Use of the PurpleAir API now requires an API Key, which must be entered in the Plugin Config dialog.
+
+To get your API Key(s), send an email to contact@purpleair.com and request API Keys.  State your First and Last Names in the email for their records.  
--- miniPurple.indigoPlugin/Contents/Server Plugin/plugin.py
+++ miniPurple.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -25,6 +25,20 @@ class Plugin(indigo.PluginBase):
 
         self.sensorDevices = {}  # Indigo device IDs, keyed by address (sensor ID)
 
+        self.apiReadKey = pluginPrefs.get("apiReadKey", None)
+        if not (self.apiReadKey and len(self.apiReadKey)):
+            self.logger.error("API Read Key must be specified in Plugin Config")
+
+        url = f"https://api.purpleair.com/v1/keys"
+        my_headers = {'X-API-Key': self.apiReadKey}
+        try:
+            response = requests.get(url, headers=my_headers)
+        except requests.exceptions.RequestException as err:
+            self.logger.error(f"{device.name}: check key RequestException: {err}")
+        else:
+            if response.json()['api_key_type'] != "READ":
+                self.logger.error("Invalid API Read Key specified in Plugin Config")
+
         self.updateFrequency = float(pluginPrefs.get('updateFrequency', "1")) * 60.0
         self.logger.debug(f"updateFrequency = {self.updateFrequency}")
         self.next_update = time.time()
@@ -52,7 +66,9 @@ class Plugin(indigo.PluginBase):
             assert device.address not in self.sensorDevices
             self.sensorDevices[device.address] = device.id
             self.logger.threaddebug(f"devices = {self.sensorDevices}")
-        
+
+        device.stateListOrDisplayStateIdChanged()
+
     def deviceStopComm(self, device):
 
         if device.deviceTypeId == 'purpleSensor':
@@ -66,59 +82,38 @@ class Plugin(indigo.PluginBase):
 
             device = indigo.devices[devID]
 
-            url = f"https://www.purpleair.com/json?show={sensorID}"
+            url = f"https://api.purpleair.com/v1/sensors/{sensorID}"
+            my_headers = {'X-API-Key': self.apiReadKey}
             try:
-                response = requests.get(url)
+                response = requests.get(url, headers=my_headers)
             except requests.exceptions.RequestException as err:
                 self.logger.error(f"{device.name}: getData RequestException: {err}")
             else:
                 self.logger.threaddebug(f"{device.name}: getData for sensor {sensorID}:\n{response.text}")
                 try:
-                    sensorData = response.json()['results'][0]
+                    sensor_data = response.json()['sensor']
                 except (Exception,):
                     self.logger.error(f"{device.name}: getData 'results' key missing: {response.text}")
                     return
 
-                try:
-                    sensorStats = json.loads(sensorData['Stats'])
-                except (Exception,):
-                    self.logger.error(f"{device.name}: getData 'Stats' key missing: {response.text}")
-                    return
-
-                sensor_aqi = int(aqi.to_iaqi(aqi.POLLUTANT_PM25, sensorData['PM2_5Value'], algo=aqi.ALGO_EPA))
+                sensor_aqi = int(aqi.to_iaqi(aqi.POLLUTANT_PM25, sensor_data['pm2.5'], algo=aqi.ALGO_EPA))
                 state_list = [
-                    {'key': 'sensorValue',  'value': sensor_aqi,               'uiValue': f"{sensor_aqi}"},
-                    {'key': 'Temperature',  'value': sensorData['temp_f'],     'decimalPlaces': 0},
-                    {'key': 'Humidity',     'value': sensorData['humidity'],   'decimalPlaces': 0},
-                    {'key': 'Pressure',     'value': sensorData['pressure'],   'decimalPlaces': 2},
-                    {'key': 'Label',        'value': sensorData['Label']},
-                    {'key': 'Latitude',     'value': sensorData['Lat']},
-                    {'key': 'Longitude',    'value': sensorData['Lon']},
-                    {'key': 'RSSI',         'value': sensorData['RSSI']},
-                    {'key': 'Uptime',       'value': sensorData['Uptime']},
-                    {'key': 'Version',      'value': sensorData['Version']},
-                    {'key': 'Hardware',     'value': sensorData['DEVICE_HARDWAREDISCOVERED']},
-                    {'key': 'p_0_3_um',     'value': sensorData['p_0_3_um']},
-                    {'key': 'p_0_5_um',     'value': sensorData['p_0_5_um']},
-                    {'key': 'p_10_0_um',    'value': sensorData['p_10_0_um']},
-                    {'key': 'p_1_0_um',     'value': sensorData['p_1_0_um']},
-                    {'key': 'p_2_5_um',     'value': sensorData['p_2_5_um']},
-                    {'key': 'p_5_0_um',     'value': sensorData['p_5_0_um']},
-                    {'key': 'pm10_0_atm',   'value': sensorData['pm10_0_atm']},
-                    {'key': 'pm10_0_cf_1',  'value': sensorData['pm10_0_cf_1']},
-                    {'key': 'pm1_0_atm',    'value': sensorData['pm1_0_atm']},
-                    {'key': 'pm1_0_cf_1',   'value': sensorData['pm1_0_cf_1']},
-                    {'key': 'pm2_5_atm',    'value': sensorData['pm2_5_atm']},
-                    {'key': 'pm2_5_cf_1',   'value': sensorData['pm2_5_cf_1']},
+                    {'key': 'sensorValue',  'value': sensor_aqi, "uiValue": f"{sensor_aqi}"},
+                    {'key': 'Temperature',  'value': sensor_data['temperature'], 'decimalPlaces': 0},
+                    {'key': 'Humidity',     'value': sensor_data['humidity'],    'decimalPlaces': 0},
+                    {'key': 'Pressure',     'value': sensor_data['pressure'],    'decimalPlaces': 2},
+                    {'key': 'Name',         'value': sensor_data['name']},
+                    {'key': 'Latitude',     'value': sensor_data['latitude']},
+                    {'key': 'Longitude',    'value': sensor_data['longitude']},
+                    {'key': 'Altitude',     'value': sensor_data['altitude']},
+                    {'key': 'RSSI',         'value': sensor_data['rssi']},
+                    {'key': 'Uptime',       'value': sensor_data['uptime']},
+                    {'key': 'Version',      'value': sensor_data['firmware_version']},
+                    {'key': 'Hardware',     'value': sensor_data['hardware']},
+                    {'key': 'pm1_0',        'value': sensor_data['pm1.0']},
+                    {'key': 'pm2_5',        'value': sensor_data['pm2.5']},
+                    {'key': 'pm10_0',       'value': sensor_data['pm10.0']},
 
-                    {'key': 'pm2_5_current','value': sensorStats['v']},
-                    {'key': 'pm2_5_10m',    'value': sensorStats['v1']},
-                    {'key': 'pm2_5_30m',    'value': sensorStats['v2']},
-                    {'key': 'pm2_5_1h',     'value': sensorStats['v3']},
-                    {'key': 'pm2_5_6h',     'value': sensorStats['v4']},
-                    {'key': 'pm2_5_24h',    'value': sensorStats['v5']},
-                    {'key': 'pm2_5_1w',     'value': sensorStats['v6']},
-                    
                 ]
                 device.updateStatesOnServer(state_list)
 
