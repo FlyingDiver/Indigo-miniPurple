df90a8f81761ef558674326764f81d4faa7f4129
Joe Keenan <joe@flyingdiver.com>
2022-05-28 09:55:07 -0400

    Cleaned up api key checks
    Added last_seen state
    Updated state names

--- miniPurple.indigoPlugin/Contents/Server Plugin/plugin.py
+++ miniPurple.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -22,35 +22,37 @@ class Plugin(indigo.PluginBase):
         self.logLevel = int(pluginPrefs.get("logLevel", logging.INFO))
         self.indigo_log_handler.setLevel(self.logLevel)
         self.logger.debug(f"logLevel = {self.logLevel}")
-        self.api_key_ok = False
 
         self.sensorDevices = {}  # Indigo device IDs, keyed by address (sensor ID)
-
-        self.apiReadKey = pluginPrefs.get("apiReadKey", None)
-        if not (self.apiReadKey and len(self.apiReadKey)):
-            self.logger.error("API Read Key must be specified in Plugin Config")
-        else:
-            url = f"https://api.purpleair.com/v1/keys"
-            my_headers = {'X-API-Key': self.apiReadKey}
-            try:
-                response = requests.get(url, headers=my_headers)
-            except requests.exceptions.RequestException as err:
-                self.logger.error(f"{device.name}: check key RequestException: {err}")
-            else:
-                if response.json()['api_key_type'] != "READ":
-                    self.logger.error("Invalid API Read Key specified in Plugin Config")
-                else:
-                    self.api_key_ok = True
 
         self.updateFrequency = float(pluginPrefs.get('updateFrequency', "1")) * 60.0
         self.logger.debug(f"updateFrequency = {self.updateFrequency}")
         self.next_update = time.time()
 
-    def startup(self):
-        self.logger.info("Starting miniPurple")
+        self.apiReadKey = pluginPrefs.get("apiReadKey", None)
+        self.api_key_ok = self.read_key_ok(self.apiReadKey)
 
-    def shutdown(self):
-        self.logger.info("Stopping miniPurple")
+    def read_key_ok(self, key) -> bool:
+        self.apiReadKey = key
+        if not (self.apiReadKey and len(self.apiReadKey)):
+            self.logger.error("API Read Key must be specified in Plugin Config")
+            return False
+
+        try:
+            response = requests.get(f"https://api.purpleair.com/v1/keys", headers={'X-API-Key': self.apiReadKey})
+        except requests.exceptions.RequestException as err:
+            self.logger.error(f"{device.name}: check key RequestException: {err}")
+            return False
+
+        try:
+            if response.json()['api_key_type'] != "READ":
+                self.logger.error("Invalid API Read Key specified in Plugin Config")
+                return False
+        except (Exception,):
+            self.logger.error("Invalid API Key request response")
+            return False
+
+        return True
 
     def runConcurrentThread(self):
         try:
@@ -85,11 +87,8 @@ class Plugin(indigo.PluginBase):
         for sensorID, devID in self.sensorDevices.items():
 
             device = indigo.devices[devID]
-
-            url = f"https://api.purpleair.com/v1/sensors/{sensorID}"
-            my_headers = {'X-API-Key': self.apiReadKey}
             try:
-                response = requests.get(url, headers=my_headers)
+                response = requests.get(f"https://api.purpleair.com/v1/sensors/{sensorID}", headers={'X-API-Key': self.apiReadKey})
             except requests.exceptions.RequestException as err:
                 self.logger.error(f"{device.name}: getData RequestException: {err}")
             else:
@@ -103,17 +102,18 @@ class Plugin(indigo.PluginBase):
                 sensor_aqi = int(aqi.to_iaqi(aqi.POLLUTANT_PM25, sensor_data['pm2.5'], algo=aqi.ALGO_EPA))
                 state_list = [
                     {'key': 'sensorValue',  'value': sensor_aqi, "uiValue": f"{sensor_aqi}"},
-                    {'key': 'Temperature',  'value': sensor_data['temperature'], 'decimalPlaces': 0},
-                    {'key': 'Humidity',     'value': sensor_data['humidity'],    'decimalPlaces': 0},
-                    {'key': 'Pressure',     'value': sensor_data['pressure'],    'decimalPlaces': 2},
-                    {'key': 'Name',         'value': sensor_data['name']},
-                    {'key': 'Latitude',     'value': sensor_data['latitude']},
-                    {'key': 'Longitude',    'value': sensor_data['longitude']},
-                    {'key': 'Altitude',     'value': sensor_data['altitude']},
-                    {'key': 'RSSI',         'value': sensor_data['rssi']},
-                    {'key': 'Uptime',       'value': sensor_data['uptime']},
-                    {'key': 'Version',      'value': sensor_data['firmware_version']},
-                    {'key': 'Hardware',     'value': sensor_data['hardware']},
+                    {'key': 'temperature',  'value': sensor_data['temperature'], 'decimalPlaces': 0},
+                    {'key': 'humidity',     'value': sensor_data['humidity'],    'decimalPlaces': 0},
+                    {'key': 'pressure',     'value': sensor_data['pressure'],    'decimalPlaces': 2},
+                    {'key': 'model',        'value': sensor_data['model']},
+                    {'key': 'latitude',     'value': sensor_data['latitude']},
+                    {'key': 'longitude',    'value': sensor_data['longitude']},
+                    {'key': 'altitude',     'value': sensor_data['altitude']},
+                    {'key': 'rssi',         'value': sensor_data['rssi']},
+                    {'key': 'uptime',       'value': sensor_data['uptime']},
+                    {'key': 'version',      'value': sensor_data['firmware_version']},
+                    {'key': 'hardware',     'value': sensor_data['hardware']},
+                    {'key': 'last_seen',    'value': time.strftime("%a, %d %b %Y %H:%M:%S",time.localtime(sensor_data['last_seen']))},
                     {'key': 'pm1_0',        'value': sensor_data['pm1.0']},
                     {'key': 'pm2_5',        'value': sensor_data['pm2.5']},
                     {'key': 'pm10_0',       'value': sensor_data['pm10.0']},
@@ -124,6 +124,18 @@ class Plugin(indigo.PluginBase):
     ########################################
     # PluginConfig methods
     ########################################
+    def validatePrefsConfigUi(self, valuesDict):
+        errorDict = indigo.Dict()
+        updateFrequency = int(valuesDict.get('updateFrequency', 15))
+        if (updateFrequency < 5) or (updateFrequency > 60):
+            errorDict['updateFrequency'] = "Update frequency is invalid - enter a valid number (between 5 and 60)"
+
+        if not self.read_key_ok(valuesDict.get("apiReadKey", None)):
+            errorDict['apiReadKey'] = "Invalid API Read Key"
+
+        if len(errorDict) > 0:
+            return False, valuesDict, errorDict
+        return True
 
     def closedPrefsConfigUi(self, valuesDict, userCancelled):
         if not userCancelled:
@@ -131,3 +143,5 @@ class Plugin(indigo.PluginBase):
             self.indigo_log_handler.setLevel(self.logLevel)
             self.logger.debug(f"logLevel = {self.logLevel}")
             self.updateFrequency = float(valuesDict.get('updateFrequency', "1")) * 60.0
+            self.apiReadKey = valuesDict.get("apiReadKey", None)
+            self.api_key_ok = self.read_key_ok(self.apiReadKey)
